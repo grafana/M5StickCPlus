1f7e2d3c12fa9bf87b9ff06ea8d85c7cdd6686ee
Emil Tullstedt <emil.tullstedt@grafana.com>
2023-05-31 11:22:38 +0200

    Add support for custom Prometheus labels

--- config.h
+++ config.h
@@ -12,6 +12,12 @@
 #define GC_USER ""
 #define GC_PASS ""
 
+// Comma-separated Prometheus labels (key=value) to apply to the metrics from the sensor
+// Example: site=home,location=balcony
+//
+// Adding labels might require you to increase the size of the WriteRequest buffer in M5StickCPlus.ino
+#define PROM_LABELS ""
+
 // Set to 1 to show debug information on the LCD screen, or 0 to not display
 // The debug information will show if you are able to connect to the WiFi or not, and whether writing the metrics to Prometheus was successful or not
 #define LCD_SHOW_DEBUG_INFO "1"
